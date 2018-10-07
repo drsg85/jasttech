9b8fae6d7f694235189100bf785d9d47848a7518
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-10-07 14:53:28 +0200

    Add sticky main nav for desktop

--- src/js/modules/DesktopMenu.js
+++ src/js/modules/DesktopMenu.js
@@ -4,11 +4,20 @@ class DesktopMenu {
         this.topPanel = document.querySelector('.hero__top');
         this.topPanel.classList.remove('hero__top--fixed');
 
+        this.hero = document.querySelector('.hero');
+
         this.events();
     }
 
     events() {
-
+        document.addEventListener('scroll', (event) => {
+            if (window.scrollY >= this.hero.offsetHeight / 2) {
+                this.topPanel.classList.add('hero__top--fixed');
+            } else {
+                this.topPanel.classList.remove('hero__top--fixed');
+            }
+            // console.log(window.scrollY, ' - ', this.hero.offsetHeight);
+        });
     }
 }
 
