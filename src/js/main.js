54bbd291badec047cf577e20d5d373ae73a04d7c
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-10-13 02:31:32 +0200

    Add redesigned device images

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ import MobileMenu from './modules/MobileMenu';
 import DesktopMenu from './modules/DesktopMenu';
 import Video from './modules/Video';
 import ContactPopup from './modules/ContactPopup';
-import Slider from './modules/Slider';
+// import Slider from './modules/Slider';
 
 new MobileMenu();
 new DesktopMenu();
 new Video();
 new ContactPopup();
-new Slider();
+// new Slider();
