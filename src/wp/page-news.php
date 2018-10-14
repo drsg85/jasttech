d931202176250c816b6bb07423352b96eb0f83f7
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-10-15 01:32:02 +0200

    Add news page wp

---
+++ src/wp/page-news.php
@@ -0,0 +1,132 @@
+<?php
+    /*
+        Template Name: News Page
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory') ?>/style.css" />
+
+    <title>Новости — Jast Charger</title>
+
+    <?php
+        wp_head();
+    ?>
+</head>
+
+<body>
+    <!-- Header -->
+    <header class="hero hero--inner">
+        <button class="menu-button">
+            <div class="menu-button__middle"></div>
+        </button>
+
+        <div class="hero__top">
+            <div class="logo">
+                <div class="logo__image">
+                    <img src="<?php bloginfo('stylesheet_directory') ?>/img/logo.svg" alt="" />
+                </div>
+                <div class="logo__text">Jast&nbsp;Charger</div>
+            </div>
+
+            <div class="hero__nav">
+                <nav class="main-nav main-nav--inv">
+                    <ul class="main-nav__list">
+                        <li class="main-nav__item">
+                            <a class="main-nav__link" href="<?php echo esc_url(home_url('/')); ?>">Франшиза</a>
+                        </li>
+                        <li class="main-nav__item">
+                            <a class="main-nav__link" href="<?php echo esc_url(home_url('/')); ?>business">Для Бизнеса</a>
+                        </li>
+                        <li class="main-nav__item">
+                            <a class="main-nav__link" href="<?php echo esc_url(home_url('/')); ?>advert">Реклама</a>
+                        </li>
+                        <li class="main-nav__item">
+                            <a class="main-nav__link" href="<?php echo esc_url(home_url('/')); ?>about">О Нас</a>
+                        </li>
+                        <li class="main-nav__item">
+                            <a class="main-nav__link" href="<?php echo esc_url(home_url('/')); ?>faq">FAQ</a>
+                        </li>
+                        <li class="main-nav__item">
+                            <a class="main-nav__link main-nav__link--current" href="<?php echo esc_url(home_url('/')); ?>news">Новости</a>
+                        </li>
+                        <li class="main-nav__item">
+                            <a class="main-nav__link" href="<?php echo esc_url(home_url('/')); ?>contacts">Контакты</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <!-- News -->
+    
+    <section class="news">
+        <header class="news__header">
+            <h2 class="section-title">Новости о самом <em>главном</em></h2>
+        </header>
+
+        <div class="news__content">
+            <?php
+                $args = array(
+                    'category_name' => 'news'
+                );
+
+                query_posts($args);
+
+                if (have_posts()) {
+                    while(have_posts()) {
+                        the_post();
+
+                        // vars
+                        $news_image     = get_field('news-image');
+            ?>
+
+                <a class="news-item" href="<?php the_permalink(); ?>">
+                    <div class="news-item__image">
+                        <img src="<?php echo $news_image; ?>" alt="<?php the_title(); ?>" />
+                    </div>
+                    <h3 class="news-item__title"><?php the_title(); ?></h3>
+                </a>
+
+            <?php
+                    }
+                }
+            ?>
+    </section>
+
+    <!-- Footer -->
+    <?php
+        get_footer();
+    ?>
+
+    <!-- Popup -->
+    <!-- <div class="contact-popup contact-popup--hidden"><button class="contact-popup__close"></button>
+        <div class="contact-popup__cover"></div>
+        <div class="contact-popup__form">
+            <script>
+                var amo_forms_params = {
+                    "id": 361897,
+                    "hash": "e7f3326ccdf13ebb3dcacd1535ecdd32",
+                    "locale": "ru"
+                };
+            </script>
+            <script id="amoforms_script" async="async" charset="utf-8" src="https://forms.amocrm.ru/forms/assets/js/amoforms.js"></script>
+        </div>
+    </div> -->
+
+    <script src="<?php bloginfo('stylesheet_directory'); ?>/faq.js"></script>
+
+    <?php
+        wp_footer();
+    ?>
+</body>
+
+</html>
