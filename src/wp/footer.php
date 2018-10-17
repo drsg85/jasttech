3cbc05906fed2f3edd1a9d7919f6815511a564fd
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-10-17 10:10:02 +0200

    Add contacts integration

--- src/wp/footer.php
+++ src/wp/footer.php
@@ -5,15 +5,16 @@
 
     <div class="footer__content">
         <div class="footer__a-n-c">
-            <p class="footer__address">Россия, г. Калининград <br>ул. Театральная, 35, офис 616 <br>(Морской Бизнес
-                Центр)</p>
+            <p class="footer__address"><?php echo the_field('c-address', 14); ?></p>
             <div class="footer__contacts">
-                <h3 class="footer__subtitle">Отдел продаж:</h3><a class="footer__phone" href="tel:+79097782086">+7
-                    909 778-20-86<br></a><a class="footer__mail" href="mailto:manager@jast.tech">manager@jast.tech</a>
+                <h3 class="footer__subtitle">Отдел продаж:</h3>
+                <a class="footer__phone" href="tel:<?php echo the_field('c-phone-link', 14); ?>"><?php echo the_field('c-phone', 14); ?></a><br>
+                <a class="footer__mail" href="mailto:<?php echo the_field('c-email', 14); ?>"><?php echo the_field('c-email', 14); ?></a>
             </div>
         </div>
-        <div class="footer__social"><a class="social social--vk" href="#">ВКонтакте</a><a class="social social--inst"
-                href="#">Инстаграм</a>
+        <div class="footer__social">
+            <a class="social social--vk" href="<?php echo the_field('c-vk', 14); ?>">ВКонтакте</a>
+            <a class="social social--inst" href="<?php echo the_field('c-instagram', 14); ?>">Инстаграм</a>
             <p class="footer__legals"><a href="#">Договор оферты</a></p>
         </div>
     </div>
